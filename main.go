e517d6fb3c3c2b28581062c8880d3dee4743d20f
sahilpatel17 <sahilofficialpatel@gmail.com>
2023-09-21 20:18:32 -0400

    sahil change

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
+
 import "fmt"
-func main()
-{
-	fmt.Println("hi")
+
+func area(length, width int) int {
+
+	Ar := length * width
+	return Ar
+}
+
+func main() {
+
+	fmt.Printf("Area of rectangle is : %d", area(12, 10))
 }
